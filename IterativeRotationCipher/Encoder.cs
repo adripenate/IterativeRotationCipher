7e3c08f1956bf8473cd4690ee294c9a44d368885
adriana <adrianapenate98@gmail.com>
2020-09-15 14:20:34 +0100

    Green test - shouldIterateOneWordTwoTimes

--- IterativeRotationCipher/Encoder.cs
+++ IterativeRotationCipher/Encoder.cs
@@ -6,8 +6,12 @@ namespace IterativeRotationCipher
     {
         public object encode(string phrase, int number_rotations)
         {
-            phrase = phrase.Substring(phrase.Length - 1) + phrase.Substring(0, phrase.Length - 1);
-            return phrase.Substring(phrase.Length - 1) + phrase.Substring(0, phrase.Length - 1);
+            for(int actual_rotation = 0; actual_rotation<number_rotations; actual_rotation++)
+            {
+                phrase = phrase.Substring(phrase.Length - number_rotations) + phrase.Substring(0, phrase.Length - number_rotations);
+                phrase = phrase.Substring(phrase.Length - number_rotations) + phrase.Substring(0, phrase.Length - number_rotations);
+            }
+            return phrase;
         }
     }
 }
