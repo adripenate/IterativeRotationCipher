37300a2a701e449bf89c120def610649a6a4a574
adriana <adrianapenate98@gmail.com>
2020-09-15 15:31:00 +0100

    Green test - ShouldIterateTwoWordsOneTime

--- IterativeRotationCipher/Encoder.cs
+++ IterativeRotationCipher/Encoder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace IterativeRotationCipher
 {
@@ -8,8 +9,20 @@ namespace IterativeRotationCipher
         {
             for(int actual_rotation = 0; actual_rotation<number_rotations; actual_rotation++)
             {
-                phrase = Rotate(phrase, number_rotations);
-                phrase = Rotate(phrase, number_rotations);
+                var phraseWithoutSpaces = Regex.Replace(phrase, @"\s+", "");
+                phraseWithoutSpaces = Rotate(phraseWithoutSpaces, number_rotations);
+                string[] original_words = phrase.Split(' ');
+                string[] words = new string[original_words.Length];
+                var separation = 0;
+                phrase = "";
+                for(int position=0; position<original_words.Length; position++)
+                {
+                    words[position] = phraseWithoutSpaces.Substring(separation, original_words[position].Length);
+                    words[position] = Rotate(words[position], number_rotations);
+                    separation += original_words[position].Length;
+                    phrase += words[position] + " ";
+                }
+                phrase = phrase.Trim();
             }
             return phrase;
         }
