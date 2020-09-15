5d95c9b426b31793756b2f18015a47a6cdcdb052
adriana <adrianapenate98@gmail.com>
2020-09-15 14:47:08 +0100

    Refactor - Code extracted as method

--- IterativeRotationCipher/Encoder.cs
+++ IterativeRotationCipher/Encoder.cs
@@ -8,10 +8,20 @@ namespace IterativeRotationCipher
         {
             for(int actual_rotation = 0; actual_rotation<number_rotations; actual_rotation++)
             {
-                phrase = phrase.Substring(phrase.Length - number_rotations) + phrase.Substring(0, phrase.Length - number_rotations);
-                phrase = phrase.Substring(phrase.Length - number_rotations) + phrase.Substring(0, phrase.Length - number_rotations);
+                phrase = Rotate(phrase, number_rotations);
+                phrase = Rotate(phrase, number_rotations);
             }
             return phrase;
+        }
+
+        private static string Rotate(string phrase, int number_rotations)
+        {
+            return phrase.Substring(GetShiftPosition(phrase, number_rotations)) + phrase.Substring(0, GetShiftPosition(phrase, number_rotations));
+        }
+
+        private static int GetShiftPosition(string phrase, int number_rotations)
+        {
+            return phrase.Length - number_rotations;
         }
     }
 }
