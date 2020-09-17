72b7191995cea16946cb1a35fabffa29e2f65c8b
adriana <adrianapenate98@gmail.com>
2020-09-17 11:01:07 +0100

    Refactor - Code extracted as method

--- IterativeRotationCipher/Encoder.cs
+++ IterativeRotationCipher/Encoder.cs
@@ -9,15 +9,15 @@ namespace IterativeRotationCipher
         {
             for(int actual_rotation = 0; actual_rotation<number_rotations; actual_rotation++)
             {
-                var phraseWithoutSpaces = Regex.Replace(phrase, @"\s+", "");
+                string phraseWithoutSpaces = RemoveSpaces(phrase);
                 phraseWithoutSpaces = Rotate(phraseWithoutSpaces, number_rotations);
                 string[] original_words = phrase.Split(' ');
                 string[] words = new string[original_words.Length];
                 var separation = 0;
                 phrase = "";
-                for(int position=0; position<original_words.Length; position++)
+                for (int position = 0; position < original_words.Length; position++)
                 {
-                    words[position] = phraseWithoutSpaces.Substring(separation, original_words[position].Length);
+                    words[position] = GetWord(phraseWithoutSpaces, original_words[position], separation);
                     words[position] = Rotate(words[position], number_rotations);
                     separation += original_words[position].Length;
                     phrase += words[position] + " ";
@@ -25,6 +25,16 @@ namespace IterativeRotationCipher
                 phrase = phrase.Trim();
             }
             return phrase;
+        }
+
+        private static string GetWord(string phraseWithoutSpaces, string original_word, int separation)
+        {
+            return phraseWithoutSpaces.Substring(separation, original_word.Length);
+        }
+
+        private static string RemoveSpaces(string phrase)
+        {
+            return Regex.Replace(phrase, @"\s+", "");
         }
 
         private static string Rotate(string phrase, int number_rotations)
