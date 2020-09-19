5286c710c4fd532c33bb46e624f3e1da9a7a7077
adriana <adrianapenate98@gmail.com>
2020-09-19 11:27:28 +0100

    Red test - ShouldDecodeOneWordOneTime

---
+++ IRCTests/DecoderTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using IterativeRotationCipher;
+
+namespace IRCTests
+{
+    public class DecoderTests
+    {
+        [Test]
+        public void ShouldDecodeOneWordOneTime()
+        {
+            var number_rotations = 1;
+            var phrase = "lohel";
+            var expectedOutput = "hello";
+
+            var output = new Decoder().Decode(phrase, number_rotations);
+
+            output.Should().BeEquivalentTo(expectedOutput);
+        }
+    }
+}
---
+++ IterativeRotationCipher/Decoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IterativeRotationCipher
+{
+    public class Decoder
+    {
+        public object Decode(string phrase, int number_rotations)
+        {
+            return "";
+        }
+    }
+}
